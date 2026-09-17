1db5cb801fa7c7e4b147dd8efc8de528d2cc56f3
Kurller <kolaquadry@gmail.com>
2026-09-18 00:13:01 +0100

    first commit

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,21 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Hospital Management API",
+        default_version="v1",
+        description="API documentation for the Hospital Management System",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -35,6 +50,12 @@ urlpatterns = [
 
     path(
         "api/appointments/",
-        include("appointments.urls")
+        include("appointments.urls"),
+    ),
+
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
     ),
 ]
